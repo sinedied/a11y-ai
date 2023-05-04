009ef84cb6562578ba14e8ad31ce224f693e37cb
sinedied <noda@free.fr>
2023-05-04 09:25:55 +0200

    feat: migrate to official @axe-core/playwright package

--- scan/base/base.spec.js
+++ scan/base/base.spec.js
@@ -1,25 +1,26 @@
 // @ts-check
 import process from 'node:process';
 import { test } from '@playwright/test';
-import { injectAxe, configureAxe, getViolations } from 'axe-playwright';
+import AxeBuilder from '@axe-core/playwright';
 
 test.describe('Accessibility', () => {
   test('get issues', async ({ page }) => {
     const url = process.env.A11Y_AI_URL || '';
     const disabledRules = process.env.A11Y_AI_DISABLED_RULES || '';
+    const axeOptions = {};
 
     await page.goto(url);
-    await injectAxe(page);
 
     if (disabledRules) {
       const rules = disabledRules
         .split(',')
         .map((rule) => rule.trim())
         .map((rule) => ({ id: rule, enabled: false }));
-      await configureAxe(page, { rules });
+      axeOptions.rules = rules;
     }
 
-    const violations = await getViolations(page, {});
+    const results = await new AxeBuilder({ page }).analyze();
+    const { violations } = results;
     console.log('===ISSUES_BEGIN===');
     console.log(JSON.stringify(violations));
     console.log('===ISSUES_END===');
